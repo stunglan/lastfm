181c66aee975caf0e534f2477c6ea0b37f777a1f
Knut Tungland <stunglan@gmail.com>
2015-07-11 14:20:03 +0200

    added lastexport.py from  web

--- analyse.R
+++ analyse.R
@@ -1,8 +1,18 @@
+setwd("~/Google Drive/src/lastfm/")
 
-
-tracks = read.delim("lastfmplayscomplete.csv",encoding="UTF-8")
+tracks = read.delim("lastfmplayscomplete.csv",quote = "",encoding="utf-8", sep = ";", header = FALSE)
 
 summary(tracks)
+
+names(tracks) <- c("title","albim","artist","date","timestamp")
+
+
+idx <- grep("^1[[:digit:]]+$",tracks$title)
+
+s <- paste(tracks$title[idx-1],tracks$albim[idx-1])
+s
+
+
 
 tracksperartist<-aggregate(tracks$playcount,list(tracks$artist),sum)
 
