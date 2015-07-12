395a37a508b8ed383412e393a8b3aee5c7197f00
Knut Tungland <stunglan@gmail.com>
2015-07-12 16:36:11 +0200

    before summer

--- analyse.R
+++ analyse.R
@@ -1,19 +1,34 @@
+
+library(dplyr)
+library(ggplot2)
+
 setwd("~/Google Drive/src/lastfm/")
 
-tracks = read.delim("lastfmplayscomplete.csv",quote = "",encoding="utf-8", sep = ";", header = FALSE)
+setwd("~/Google Drive/src/lastfm/")
 
-summary(tracks)
+tracks = read.delim("exported_tracks.txt",quote = "", encoding="utf-8", sep = "\t", header = FALSE,stringsAsFactors = FALSE)
+names(tracks) <- c("timestamp","title","artist","album", "trackmbid", "artistmbid", "albummbid")
+tracks$date <- as.POSIXct(tracks$timestamp, origin="1970-01-01")
+tracks$year <- as.numeric(format(as.POSIXct(tracks$timestamp,origin="1970-01-01"),format="%Y"))
+tracks$month <- as.numeric(format(as.POSIXct(tracks$timestamp,origin="1970-01-01"),format="%m"))
+tracks$day <- as.numeric(format(as.POSIXct(tracks$timestamp,origin="1970-01-01"),format="%d"))
+tracks$weekday <- format(as.POSIXct(tracks$timestamp,origin="1970-01-01"),format="%a")
 
-names(tracks) <- c("title","albim","artist","date","timestamp")
+str(tracks)
 
+head(tracks,15)
+nrow(tracks)
 
-idx <- grep("^1[[:digit:]]+$",tracks$title)
+summary()
 
-s <- paste(tracks$title[idx-1],tracks$albim[idx-1])
-s
+head(select(filter(tracks, year==2013, month==07 ), title, album, artist),15)
 
+by_month <- group_by(tracks, year, month )
 
+plays <- summarise(by_month,
+                   count = n(),
+                   n_distinct(artist),
+                   first(title)
+                   )
+plot(plays)
 
-tracksperartist<-aggregate(tracks$playcount,list(tracks$artist),sum)
-
-names(tracksperartist) <- c("artist","playcount")
--- cleandata.R
+++ cleandata.R
@@ -1,2 +1,26 @@
 # clean the data
 
+
+setwd("~/Google Drive/src/lastfm/")
+
+tracks = read.delim("exported_tracks.txt",quote = "", encoding="utf-8", sep = "\t", header = FALSE,stringsAsFactors = FALSE)
+names(tracks) <- c("timestamp","title","artist","album", "trackmbid", "artistmbid", "albummbid")
+summary(tracks)
+
+
+
+# find the lines with extra 
+
+idx <- grep("^1[[:digit:]]+$",tracks$title)
+tracks$title[1]
+as.POSIXct(tracks$timestamp[1], origin="1970-01-01")
+
+
+tracks$date <- as.POSIXct(tracks$timestamp, origin="1970-01-01")
+
+
+for (i in idx)  {
+  print(c(tracks$title[i],tracks$album[i],tracks$artist[i],tracks$date[i]) )
+}
+
+
